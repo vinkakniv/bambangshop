2daaff9ed24d9ccf48da88e9f460b3a2adf35a93
vinkakniv <vinka.aeris@gmail.com>
2024-04-10 11:42:40 +0700

    Implement subscribe function in Notification controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -3,11 +3,12 @@ pub mod notification;
 
 use rocket::fairing::AdHoc;
 
+
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
             .mount("/product", routes![product::create, product::list, product::read, product::delete])
-            .mount("/notification", routes![])
+            .mount("/notification", routes![notification::subscribe])
     });
 }
 
